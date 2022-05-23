ca7a2f581be4aa1aaf6eedaf86de517a7fccbca0
Silv <forenks1005@gmail.com>
2022-05-23 17:40:36 -0500

    Adding changes

---
+++ index.js
@@ -0,0 +1,66 @@
+window.sr =ScrollReveal();
+
+sr.reveal('#first', {
+   duration: 3000,
+   origin:'top',
+   distance: '-100px',
+   easing: 'cubic-bezier(0.1, 0, 0, 1)',
+   
+});
+
+sr.reveal('#second', {
+    duration: 3000,
+    origin:'top',
+    distance: '-100px',
+    easing: 'cubic-bezier(0.1, 0, 0, 1)',
+    delay: '100',
+ });
+ 
+ sr.reveal('#third', {
+    duration: 3000,
+    origin:'top',
+    distance: '-100px',
+    easing: 'cubic-bezier(0.1, 0, 0, 1)',
+    delay: '200',
+ });
+
+ sr.reveal('#fourth', {
+    duration: 3000,
+    origin:'top',
+    distance: '-100px',
+    easing: 'cubic-bezier(0.1, 0, 0, 1)',
+    delay: '200',
+ });
+
+
+ sr.reveal('#twitter', {
+    duration: 3000,
+    origin:'right',
+    distance: '-50px',
+    easing: 'cubic-bezier(0.1, 0, 0, 1)',
+    delay: '200',
+ });
+
+
+ sr.reveal('#ig', {
+    duration: 3000,
+    origin:'top',
+    distance: '-50px',
+    easing: 'cubic-bezier(0.1, 0, 0, 1)',
+    delay: '200',
+ });
+
+
+        sr.reveal('#git-hub', {
+    duration: 3000,
+    origin:'left',
+    distance: '-50px',
+    easing: 'cubic-bezier(0.1, 0, 0, 1)',
+    delay: '200',
+ });
+
+
+
+
+
+
